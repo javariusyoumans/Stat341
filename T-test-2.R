5798d6529add9ae2caf6bf1c9e837ebc7eb0e234
javariusyoumans <javarius.youmans@gmail.com>
2025-10-23 16:11:08 -0400

    Add files via upload

---
+++ T-test-2.R
@@ -0,0 +1,29 @@
+#Author: Youmans, Javarius; Date: 10/21/2025; Purpose: Calculate T-test 2
+
+#Create some dummy dataset for 2 variables
+x = rnorm(10)
+y = rnorm(10)
+
+
+# Create a density plot for x & y
+pts = seq(-4.5,4.5,length=100)
+plot(pts,dt(pts,df=9),col='red',type='l')
+lines(density(x), col='green')
+lines(density(y), col='blue')
+
+# Calculate T test
+ttest = t.test(x,y)
+ttest
+
+#T-test output
+
+Welch Two Sample t-test
+
+data:  x and y
+t = -0.96313, df = 14.424, p-value = 0.3514
+alternative hypothesis: true difference in means is not equal to 0
+95 percent confidence interval:
+ -1.472293  0.558044
+sample estimates:
+ mean of x  mean of y 
+-0.9206154 -0.4634911 
